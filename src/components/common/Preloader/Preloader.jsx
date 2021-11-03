8aea72cb4b55fcf889d5f22df7a61637f999758d
YevgeniyBukurIT <85176058+YevgeniyBukurIT@users.noreply.github.com>
2021-11-03 19:03:24 +0200

    92 Lesson. New Preloader

--- src/components/common/Preloader/Preloader.jsx
+++ src/components/common/Preloader/Preloader.jsx
@@ -1,9 +1,10 @@
-import React from "react";
-import s from "../../Users/User.module.css";
-import spinner from "../../../assets/images/Spinner.gif";
+import React from 'react'
+import s from './Preloader.module.css'
+import spinner from '../../../assets/images/Preloader.gif'
 
-const Preloader = (props) =>{
+const Preloader = (props) => {
     return <img className={s.imgPreloader} src={spinner}/>
+
 }
 
 export default Preloader
